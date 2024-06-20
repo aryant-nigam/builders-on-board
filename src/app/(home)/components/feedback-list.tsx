53960bac5f50899aa38840a6dcf0a3a695fc8125
aryant-nigam <aryantnigam@gmail.com>
2024-06-20 09:40:34 +0530

    added slider to feedback list

--- src/app/(home)/components/feedback-list.tsx
+++ src/app/(home)/components/feedback-list.tsx
@@ -5,9 +5,13 @@ import FeedbackCard from "./feedback-card";
 import useHttp from "@/hooks/use-http";
 import { toTitleCase } from "@/utils";
 import Loader from "@/components/loader";
+import { Button } from "@mui/material";
 
 function FeedbackList() {
   const [feedbackCardsList, setFeedbackCardsList] = useState([]);
+  const [feedbackContainer, setFeedbackContainer] = useState<Element | null>(
+    null
+  );
   const { get, isLoading, successMsg, errorMsg } = useHttp(
     `https://builders-on-board-be-2.onrender.com/feedbacks`
   );
@@ -17,9 +21,10 @@ function FeedbackList() {
       const response = await get(null);
       console.log(response);
       if (response.length > 0) {
-        const list = response.map((feedback: any) => {
+        const list = response.map((feedback: any, index: number) => {
           return (
             <FeedbackCard
+              key={index}
               customerName={`${toTitleCase(
                 feedback.customer_firstname
               )} ${toTitleCase(feedback.customer_lastname)}`}
@@ -36,13 +41,67 @@ function FeedbackList() {
     getFeedbacks();
   }, []);
 
+  useEffect(() => {
+    setFeedbackContainer(
+      document.querySelector(`#${classes["feedback-container"]}`)
+    );
+    console.log(document.querySelector(`#${classes["feedback-container"]}`));
+  }, []);
+
+  const moveNextHandler = () => {
+    if (feedbackContainer) {
+      let width = feedbackContainer.clientWidth;
+      feedbackContainer.scrollLeft = feedbackContainer.scrollLeft + width;
+    }
+  };
+
+  const movePrevHandler = () => {
+    if (feedbackContainer) {
+      let width = feedbackContainer.clientWidth;
+      feedbackContainer.scrollLeft = feedbackContainer.scrollLeft - width;
+    }
+  };
+
   return (
-    <div className={classes["feedback-list"]}>
-      {feedbackCardsList.length > 0 && feedbackCardsList}
+    <div className={classes["feedback-carousel"]}>
+      {/* {feedbackCardsList.length > 0 && feedbackCardsList}
       {isLoading && <Loader />}
       {feedbackCardsList.length == 0 && !isLoading && (
         <p>No feedbacks to show up</p>
-      )}
+      )} */}
+      <button
+        className={`${classes["prev-btn"]} ${
+          feedbackCardsList.length <= 3 && classes.hidden
+        }`}
+      >
+        <img
+          src="arrow.png"
+          className={classes["prev-icon"]}
+          onClick={movePrevHandler}
+        ></img>
+      </button>
+      <div id={classes["feedback-container"]}>
+        {feedbackCardsList.length > 0 && feedbackCardsList}
+        {
+          <div className={classes["loader-container"]}>
+            {isLoading && <Loader />}
+            {feedbackCardsList.length == 0 && !isLoading && (
+              <p className={classes["error-msg"]}>No feedbacks to show up</p>
+            )}
+          </div>
+        }
+      </div>
+      <button
+        className={`${classes["next-btn"]} ${
+          feedbackCardsList.length <= 3 && classes.hidden
+        }`}
+      >
+        <img
+          src="arrow.png"
+          className={classes["next-icon"]}
+          onClick={moveNextHandler}
+        ></img>
+      </button>
     </div>
   );
 }
